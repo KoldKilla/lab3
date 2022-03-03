3fc461a44d3adca19c292e4934c68ec67379224f
Konstantin <undeadextreme@mail.ru>
2022-03-03 23:55:40 +0300

    Initial commit

---
+++ Point3d/src/com/company/Point3d.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Point3d extends Point2d{
+
+    public {
+	// write your code here
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Введите координаты Х, Y, Z для первой точки:");
+        Scanner sx1 = new Scanner(System.in);
+        Scanner sy1 = new Scanner(System.in);
+        Scanner sz1 = new Scanner(System.in);
+
+        double x1 = sx1.nextDouble();
+        double y1 = sy1.nextDouble();
+        double z1 = sz1.nextDouble();
+
+        System.out.println("Введите координаты Х, Y, Z для второй точки:");
+        Scanner sx2 = new Scanner(System.in);
+        Scanner sy2 = new Scanner(System.in);
+        Scanner sz2 = new Scanner(System.in);
+
+        double x2 = sx2.nextDouble();
+        double y2 = sy2.nextDouble();
+        double z2 = sz2.nextDouble();
+
+        System.out.println("Введите координаты Х, Y, Z для третьей точки:");
+        Scanner sx3 = new Scanner(System.in);
+        Scanner sy3 = new Scanner(System.in);
+        Scanner sz3 = new Scanner(System.in);
+
+        double x3 = sx3.nextDouble();
+        double y3 = sy3.nextDouble();
+        double z3 = sz3.nextDouble();
+
+        Point3d point1 = new Point3d(x1, y1, z1);
+        Point3d point2 = new Point3d(x2, y2, z2);
+        Point3d point3 = new Point3d(x3, y3, z3);
+
+        Point3d point4 = new Point3d();
+
+        if (!point1.isEqual(point2) && !point2.isEqual(point3) && !point1.isEqual(point3))
+        {
+            System.out.println("Полученная площадь:");
+            System.out.println(Point3d.computerArea(point1, point2, point3));
+        }
+        else
+            System.out.println("Вы ввели неверные данные!");
+    }
+}
